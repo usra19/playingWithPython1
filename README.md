1bf91628f79c4e5c82c9d7835dcde72cf93ca9ac
Usman Rafiq <47300588+usra19@users.noreply.github.com>
2024-04-17 18:55:27 +0200

    First few concepts implemented

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # playingWithPython1
 The first testing project 
+learnt the following:
+
+-input
+- print
+- -data types
+- type casting int, string, float etc
+- arrays
+- objects
+- loops
+- if/else
+- range
--- helloPython.py
+++ helloPython.py
@@ -1,1 +1,33 @@
 
+
+#
+#1: We check in a patient named John Smith. He is 20 years and a new patient
+#
+#E1 Solution
+
+name= "Jogn Smith"
+age=20
+isnewPatient=True
+print("Name:"+name+"\nAge:"+str(age)+"\nIs New Patient:"+str(isnewPatient))
+
+
+#
+#2: Three types of input: numbers, strings and boolean
+# input birth year and give age
+#E2 Solution
+
+#birth_year=input("Please input your birth year:")
+#input is always in string
+#age=2024-int(birth_year)
+#print("Age:"+str(age))
+
+#
+#3: If statements
+#E3
+
+temprature=35
+
+if temprature>30: #we use : to represent a block of code
+    print("It's 'a hot day")
+else:
+    print("It's not a hod day")
